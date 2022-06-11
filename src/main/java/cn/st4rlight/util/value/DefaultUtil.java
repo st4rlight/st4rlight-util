55d5292c2b7bc2d498788f1205a572e3a3864a9b
st4rlight <st4rlight@163.com>
2022-06-12 00:03:24 +0800

    添加null则获取值

--- src/main/java/cn/st4rlight/util/value/DefaultUtil.java
+++ src/main/java/cn/st4rlight/util/value/DefaultUtil.java
@@ -3,6 +3,7 @@ package cn.st4rlight.util.value;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -16,23 +17,30 @@ import com.google.common.collect.Lists;
  */
 public class DefaultUtil {
 
+    /**
+     * null则转换成各类型默认值
+     */
     public static Long nullToDefault(Long value) {
         return Objects.nonNull(value) ? value : 0L;
     }
-
     public static String blankToDefault(String value) {
         return StringUtils.defaultIfBlank(value, StringUtils.EMPTY);
     }
-
     public static <T> List<T> nullToDefault(List<T> list) {
         return Objects.nonNull(list) ? list : Lists.newArrayList();
     }
-
     public static <T> T nullToDefault(T value, T defaultValue) {
         return Objects.nonNull(value) ? value : defaultValue;
     }
 
-    public static <T, R> R getOrDefault(T t, Function<T, R> function, R defaultValue) {
-        return Objects.nonNull(t) ? function.apply(t) : defaultValue;
+
+    /**
+     * null则获取值
+     */
+    public static <T> T nullOrSupply(T t, Supplier<T> supplier) {
+        return Objects.nonNull(t) ? t : supplier.get();
+    }
+    public static <T, R> R getOrDefault(T t, Function<T, R> getter, R defaultValue) {
+        return Objects.nonNull(t) ? getter.apply(t) : defaultValue;
     }
 }
