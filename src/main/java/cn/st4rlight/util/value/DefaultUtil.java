30b37cda799eba28e836fae529855cd73f984dc6
st4rlight <st4rlight@163.com>
2022-06-11 23:53:59 +0800

    引入工具类maven，添加默认值工具类

--- src/main/java/cn/st4rlight/util/value/DefaultUtil.java
+++ src/main/java/cn/st4rlight/util/value/DefaultUtil.java
@@ -1,5 +1,13 @@
 package cn.st4rlight.util.value;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.collect.Lists;
+
 /**
  * 默认值
  *
@@ -7,4 +15,24 @@ package cn.st4rlight.util.value;
  * Created on 2022-06-11
  */
 public class DefaultUtil {
+
+    public static Long nullToDefault(Long value) {
+        return Objects.nonNull(value) ? value : 0L;
+    }
+
+    public static String blankToDefault(String value) {
+        return StringUtils.defaultIfBlank(value, StringUtils.EMPTY);
+    }
+
+    public static <T> List<T> nullToDefault(List<T> list) {
+        return Objects.nonNull(list) ? list : Lists.newArrayList();
+    }
+
+    public static <T> T nullToDefault(T value, T defaultValue) {
+        return Objects.nonNull(value) ? value : defaultValue;
+    }
+
+    public static <T, R> R getOrDefault(T t, Function<T, R> function, R defaultValue) {
+        return Objects.nonNull(t) ? function.apply(t) : defaultValue;
+    }
 }
